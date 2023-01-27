a2487ea178ddf9c79117f4a095fea87daf3fcd90
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2023-01-27 15:30:38 -0500

    Feat/create navbar (#3)
    * Feat/add basic conf (#1)
    
    * feat: set scaffolding BEM
    
    * fix: delete vue logo
    
    * feat: conf Bulma
    
    ---------
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>
    
    * feat: creates navbar
    
    ---------
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <home-main />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeMain from '@/components/home/HomeMain.vue';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  components: { HomeMain },
 };
 </script>
