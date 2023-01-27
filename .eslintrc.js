0d004a92f73494b919747261959a3b7524826fe1
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2023-01-27 12:07:58 -0500

    Feat/add basic conf (#1)
    * feat: set scaffolding BEM
    
    * fix: delete vue logo
    
    * feat: conf Bulma
    
    ---------
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,21 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
   },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
